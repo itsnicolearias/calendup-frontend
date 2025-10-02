fc7a47f3a83f36d9a101ed65223eacba5db48439
itsnicolearias <nicolmccann@gmail.com>
2025-10-02 12:08:18 -0300

    feat: allow to use facebook login

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -174,6 +174,7 @@ const handleSubmit = async (e: React.FormEvent) => {
             </div>
 
             <div className="space-y-3">
+              {/* google */}
               <Button
                 type="button"
                 variant="outline"
@@ -200,7 +201,25 @@ const handleSubmit = async (e: React.FormEvent) => {
                 </svg>
                 Continuar con Google
               </Button>
-
+              
+              {/* facebook */}
+              <Button
+                type="button"
+                variant="outline"
+                onClick={() => handleSocialLogin("facebook")}
+                //disabled={true}
+                className="w-full h-12 border-gray-200 hover:bg-gray-50 transition-colors duration-200"
+              >
+                <svg className="w-5 h-5 mr-3" viewBox="0 0 24 24">
+                  <path
+                    fill="#1877F2"
+                    d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
+                  />
+                </svg>
+                Continuar con Facebook
+              </Button>
+              
+              {/* microsoft */}
               <Button
                 type="button"
                 variant="outline"
@@ -217,21 +236,7 @@ const handleSubmit = async (e: React.FormEvent) => {
                 Continuar con Hotmail
               </Button>
 
-              <Button
-                type="button"
-                variant="outline"
-                onClick={() => handleSocialLogin("facebook")}
-                disabled={true}
-                className="w-full h-12 border-gray-200 hover:bg-gray-50 transition-colors duration-200"
-              >
-                <svg className="w-5 h-5 mr-3" viewBox="0 0 24 24">
-                  <path
-                    fill="#1877F2"
-                    d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
-                  />
-                </svg>
-                Continuar con Facebook
-              </Button>
+             
             </div>
 
             <div className="text-center">
